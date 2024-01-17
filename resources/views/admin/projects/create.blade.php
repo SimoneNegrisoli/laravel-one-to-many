ac9a3d12fd3e7955ccef03862e37beb8f5420184
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-17 18:28:21 +0100

    type select

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -4,12 +4,29 @@
         <h2>New Project</h2>
         <form action="{{ route('admin.projects.store') }}" enctype="multipart/form-data" method="POST">
             @csrf
+
+            {{-- TITLE --}}
             <div class="mb-3">
-                {{-- TITLE --}}
                 <label for="title">Titolo</label>
                 <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
                     required maxlength="200" minlength="3" value="{{ old('title') }}">
                 @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            {{-- TYPE --}}
+            <div class="mb-3">
+
+                <label for="type_id">Select Type</label>
+                <select class="form-control @error('type_id') is-invalid @enderror" name="type_id" id="type_id">
+                    <option value=""></option>
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}" {{ old('type_id') == $type->id ? 'selected' : '' }}>
+                            {{ $type->name }}</option>
+                    @endforeach
+                </select>
+                @error('type_id')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -5,6 +5,8 @@
         <form action="{{ route('admin.projects.update', $project->slug) }}" enctype="multipart/form-data" method="project">
             @csrf
             @method('PUT')
+
+            {{-- TITLE --}}
             <div class="mb-3">
                 <label for="title">Title</label>
                 <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
@@ -14,6 +16,24 @@
                 @enderror
             </div>
 
+            {{-- TYPE --}}
+            <div class="mb-3">
+
+                <label for="type_id">Select Type</label>
+                <select class="form-control @error('type_id') is-invalid @enderror" name="type_id" id="type_id">
+                    <option value=""></option>
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}"
+                            {{ old('type_id', $project->type_id) == $type->id ? 'selected' : '' }}>
+                            {{ $type->name }}</option>
+                    @endforeach
+                </select>
+                @error('type_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            {{-- BODY --}}
             <div class="mb-3">
                 <label for="body">Body</label>
                 <textarea class="form-control @error('body') is-invalid @enderror" name="body" id="body" cols="30"
@@ -25,6 +45,7 @@
                 @enderror
             </div>
 
+            {{-- IMG --}}
             <div class="mb-3">
                 <label for="image">Image</label>
                 <input type="file" class="form-control @error('image') is-invalid @enderror" name="image"
@@ -33,6 +54,8 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
+
+            {{-- BTN --}}
             <button type="submit" class="btn btn-success">Save</button>
             <button type="reset" class="btn btn-primary">Reset</button>
 
