692571e81be57a873e6454ef3ddc12106d70a5c2
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-17 16:54:27 +0100

    seeding type seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['Fullstack', 'Front-end', 'Back-end', 'Front-office', 'Back-office'];
+        foreach ($types as $type) {
+            $newType = new Type();
+
+            $newType->name = $type;
+            $newType->slug = Str::slug($type, '-');
+
+            $newType->save();
+        }
     }
 }
